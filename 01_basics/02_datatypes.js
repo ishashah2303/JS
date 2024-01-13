478c5b33a218b66c811431d1cca3d9dba48974c3
ishashah23 <ishashah2004@hotmail.com>
2024-01-13 23:17:34 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs and not browser
+
+console.log(3
+    +
+    3)  //code readability should be high
+console.log("ISHA")
+
+/* DOCUMENTATION : https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+                   https://tc39.es/ecma262/
+*/
+
+let name="isha"
+let age=19
+let isLoggedIn=true
+ // null- standalone value  when it comes to weather 0 cant be used so Null 
+
+ //interview question-- null is an object but undefined is undefined
