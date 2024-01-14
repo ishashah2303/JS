592851a4451a5043b2323e7a8d2e39651687fe77
ishashah23 <ishashah2004@hotmail.com>
2024-01-14 16:47:19 +0530

    String methods

---
+++ 01_basics/04_string.js
@@ -0,0 +1,17 @@
+const name="ISHA"
+const repoCount=50
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String('ABcdefgh')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.toLowerCase())
+console.log(gameName.slice(-8,4))
+
+const newString="   isha   "
+console.log(newString)
+console.log(newString.trim())
+
+const newString1="isha-shah-23"
+console.log(newString1.split("-"))
