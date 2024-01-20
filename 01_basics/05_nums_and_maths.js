1ac0795aed29c96a39473770a9e7148f0574c12e
ishashah23 <ishashah2004@hotmail.com>
2024-01-20 16:05:40 +0530

    Numbers and Maths

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,29 @@
+const score=400
+//console.log(score)
+const balance=new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const no1= 1123.8966
+//console.log(no1.toPrecision(3))
+
+const no2= 10000000
+//console.log(no2.toLocaleString())
+
+
+
+/////MATHS/////
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.4))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
